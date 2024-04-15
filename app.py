633e0dd5daf1a41c83cf92909b747ba7f9cd9e5b
poqop721 <poqop721@naver.com>
2024-04-15 20:36:09 +0900

    fix : fixing ec2 chrome not working7

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ def search_category():
    print(num_of_page, check)
    if num_of_page != 0 : # beautifulsoup 네이버 막힘
       driver = webdriver.Chrome(options=chrome_options)
-      print(url)
-      driver.get(url)
       print(driver)
+      driver.get(url)
+      print('pass')
       time.sleep(1)
       driver.refresh()
       body = driver.find_element(By.TAG_NAME, "body")
