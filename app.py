b66029bddaff64efb7c9b0bcdbca06d6897d6b01
poqop721 <poqop721@naver.com>
2024-04-20 20:59:53 +0900

    fix : testing ec2 env

--- app.py
+++ app.py
@@ -250,9 +250,10 @@ class ChromeDriver:
          self.event.wait()
          if self.done == False:
             print(f'\n<failed driver.get - retrying ({count}/{try_num})>')
-            thread.join()
+            thread.join() 
             count = count + 1
          else :
+            print('fin')
             thread.join()
             break
 
